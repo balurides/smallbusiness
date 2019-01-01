472e0b6b5698d8ffeb1f226a48c9dd6366f2d693
Balaji.Gandreti <Balaji.Gandreti@bestbuy.com>
2018-12-31 21:20:00 -0600

    updated contact us

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 export default class Contactus extends Component {
     render() {
         return(
-            <div className="contactus-container">
-                <img id = "contactus-img" src="./src/images/contactus.jpg" /> */}
-                <div className="inside-bar"> contact us</div>
+            <div>
+                <div className="contactus-container">
+                    <div className="contactus-img"> contact us</div>
+                </div>
+
             </div>
         )
     }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,32 @@
 import React, { Component  } from 'react';
+import './../../style/footer.css';
 
 export default class Footer extends Component {
     render() {
         return(
             <footer>
-            <div className="footer" > 
-             footer test 
-                <li> footer</li>
-                <li> is</li>
-                <li> centered</li>
+                <div className="footer" > 
+                    <ul>
+                        <li> 
+                            <div> about morning person </div>
+                            <div>test 1</div>
+                            <div>test 1</div>
+                            <div>test 1</div>
+                        </li>
+                        <li> 
+                            <div> what more  </div>
+                            <div>test 1</div>
+                            <div>test 1</div>
+                            <div>test 1</div>
+                        </li>
+                        <li> 
+                            <i className="support-images"> </i>
+                                <h4> Talk to us  </h4>
+                                <div>test 1</div>
+                                <div>test 1</div>
+                                <div>test 1</div>
+                        </li>
+                    </ul>
                 </div>
             </footer>
            
